15392cdbc8e231e4e260ab91afb08750a5a75127
Ramos <beidseitig@gmail.com>
2022-06-05 21:41:30 -0300

    feat: 100% do requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -8,7 +8,8 @@ CREATE TABLE SpotifyClone.artists(
     CONSTRAINT PRIMARY KEY (artist_id)
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.artists (artist_name) VALUES
+INSERT INTO SpotifyClone.artists (artist_name)
+VALUES
 	('Walter Phoenix'),
     ('Peter Strong'),
     ('Lance Day'),
@@ -25,7 +26,8 @@ CREATE TABLE SpotifyClone.albuns(
     FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.albuns (album_name, artist_id, year_released) VALUES
+INSERT INTO SpotifyClone.albuns (album_name, artist_id, year_released)
+VALUES
 	('Envious',1,1990),
     ('Exuberant',1,1993),
     ('Hallowed Steam',2,1995),
@@ -46,7 +48,8 @@ CREATE TABLE SpotifyClone.musics(
     FOREIGN KEY (album_id) REFERENCES albuns (album_id)
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.musics (album_id, music_name, music_duration) VALUES
+INSERT INTO SpotifyClone.musics (album_id, music_name, music_duration)
+VALUES
 	(1,'Soul For Us',200),
     (1,'Reflections Of Magic',163),
     (1,'Dance With Her Own',116),
@@ -95,8 +98,116 @@ CREATE TABLE SpotifyClone.plans(
     CONSTRAINT PRIMARY KEY (plan_id)
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.plans (plan_name,plan_value) VALUES
+INSERT INTO SpotifyClone.plans (plan_name,plan_value)
+VALUES
     ('gratuito', '0'),
     ('familiar', '7.99'),
     ('universitário', '5.99'),
     ('pessoal', '6.99');
+    
+CREATE TABLE SpotifyClone.users(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    plan_id INT NOT NULL,
+    plan_date DATE NOT NULL,
+    CONSTRAINT PRIMARY KEY (user_id),
+    FOREIGN KEY (plan_id) REFERENCES plans (plan_id)
+) engine = InnoDB;
+    
+INSERT INTO SpotifyClone.users (user_name,age,plan_id,plan_date)
+VALUES
+    ('Thati', '23', '1', '2019-10-20'),
+    ('Cintia', '35', '2', '2017-12-30'),
+    ('Bill', '20', '3', '2019-06-05'),
+    ('Roger', '45', '4', '2020-05-13'),
+    ('Norman', '58', '4', '2017-02-17'),
+    ('Patrick', '33', '2', '2017-01-06'),
+    ('Vivian', '26', '3', '2018-01-05'),
+    ('Carol', '19', '3', '2018-02-14'),
+    ('Angelina', '42', '2', '2018-04-29'),
+    ('Paul', '46', '2', '2017-01-17');
+
+CREATE TABLE SpotifyClone.followers(
+    user_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (user_id, artist_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+) engine = InnoDB;
+
+INSERT INTO SpotifyClone.followers (user_id, artist_id)
+VALUES
+    ('1', '1'),
+    ('1', '4'),
+    ('1', '3'),
+    ('2', '1'),
+    ('2', '3'),
+    ('3', '2'),
+    ('3', '1'),
+    ('4', '4'),
+    ('5', '5'),
+    ('5', '6'),
+    ('6', '6'),
+    ('6', '3'),
+    ('6', '1'),
+    ('7', '2'),
+    ('7', '5'),
+    ('8', '1'),
+    ('8', '5'),
+    ('9', '6'),
+    ('9', '4'),
+    ('9', '3'),
+    ('10', '2'),
+    ('10', '6');
+
+CREATE TABLE SpotifyClone.history(
+    user_id INT NOT NULL,
+    music_id INT NOT NULL,
+    reproduction_date DATETIME NOT NULL,
+    CONSTRAINT PRIMARY KEY (user_id, music_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (music_id) REFERENCES musics (music_id)
+) engine = InnoDB;
+
+INSERT INTO SpotifyClone.history (user_id,music_id,reproduction_date)
+VALUES
+    ('1', '36', '2020-02-28 10:45:55'),
+    ('1', '25', '2020-05-02 05:30:35'),
+    ('1', '23', '2020-03-06 11:22:33'),
+    ('1', '14', '2020-08-05 08:05:17'),
+    ('1', '15', '2020-09-14 16:32:22'),
+    ('2', '34', '2020-01-02 07:40:33'),
+    ('2', '24', '2020-05-16 06:16:22'),
+    ('2', '21', '2020-10-09 12:27:48'),
+    ('2', '39', '2020-09-21 13:14:46'),
+    ('3', '6', '2020-11-13 16:55:13'),
+    ('3', '3', '2020-12-05 18:38:30'),
+    ('3', '26', '2020-07-30 10:00:00'),
+    ('4', '2', '2021-08-15 17:10:10'),
+    ('4', '35', '2021-07-10 15:20:30'),
+    ('4', '27', '2021-01-09 01:44:33'),
+    ('5', '7', '2020-07-03 19:33:28'),
+    ('5', '12', '2017-02-24 21:14:22'),
+    ('5', '14', '2020-08-06 15:23:43'),
+    ('5', '1', '2020-11-10 13:52:27'),
+    ('6', '38', '2019-02-07 20:33:48'),
+    ('6', '29', '2017-01-24 00:31:17'),
+    ('6', '30', '2017-10-12 12:35:20'),
+    ('6', '22', '2018-05-29 14:56:41'),
+    ('7', '5', '2018-05-09 22:30:49'),
+    ('7', '4', '2020-07-27 12:52:58'),
+    ('7', '11', '2018-01-16 18:40:43'),
+    ('8', '39', '2018-03-21 16:56:40'),
+    ('8', '40', '2020-10-18 13:38:05'),
+    ('8', '32', '2019-05-25 08:14:03'),
+    ('8', '33', '2021-08-15 21:37:09'),
+    ('9', '16', '2021-05-24 17:23:45'),
+    ('9', '17', '2018-12-07 22:48:52'),
+    ('9', '8', '2021-03-14 06:14:26'),
+    ('9', '9', '2020-04-01 03:36:00'),
+    ('10', '20', '2017-02-06 08:21:34'),
+    ('10', '21', '2017-12-04 05:33:43'),
+    ('10', '12', '2017-07-27 05:24:49'),
+    ('10', '13', '2017-12-25 01:03:57');
+    
