b4cff160e939573a6a7e1e905fa760d49ed9f756
Ramos <beidseitig@gmail.com>
2022-06-06 20:54:05 -0300

    feat: requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT
+	users.user_name AS usuario,
+    IF(MAX(YEAR(history.reproduction_date)) = 2021,
+		'Usuário ativo',
+        'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.users AS users
+INNER JOIN SpotifyClone.history AS history ON users.user_id = history.user_id
+GROUP BY usuario
+ORDER BY usuario;
