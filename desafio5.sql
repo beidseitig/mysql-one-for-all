43138d1048eae4b345049991bcbc495dbce8e5cf
Ramos <beidseitig@gmail.com>
2022-06-07 21:16:53 -0300

    feat: requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT
+	musics.music_name AS cancao,
+    COUNT(history.music_id) AS reproducoes
+FROM SpotifyClone.musics AS musics
+INNER JOIN SpotifyClone.history AS history ON musics.music_id = history.music_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
